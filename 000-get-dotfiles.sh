1c679267f8ce07776c053b60c64f6d1ed6751e5e
Roberto Ughi <umbe77@gmail.com>
2020-08-06 02:33:42 +0200

    Update 000-get-dotfiles.sh
    Added .config/gtk-3.0/settings.ini to baked up files

--- 000-get-dotfiles.sh
+++ 000-get-dotfiles.sh
@@ -4,6 +4,7 @@ cd ~
 #bakup file 
 mv .bashrc .bashrc.fedora
 mv .bash_profile .bash_profile.fedora
+mv .config/gtk-3.0/settings.ini .config/gtk-3.0/settings.ini.fedora
 #set bare repo
 git clone --bare https://github.com/umbe77/dotfiles.git $HOME/.dotfiles
 #checkout bare dotfiles repo
