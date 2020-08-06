d0e35c012e3b8cd28173d4b771e8f60da72f1cad
Roberto Ughi <umbe77@gmail.com>
2020-08-06 03:08:08 +0200

    Update 003-python.sh
    add install for ranger devicons

--- 003-python.sh
+++ 003-python.sh
@@ -10,3 +10,5 @@ echo "Installing xparser module"
 sudo pip3 install xparser
 echo "Installing neovim python provider"
 python3 -m pip install --user --upgrade pynvim
+echo "Installing dev icons for ranger fm"
+git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
